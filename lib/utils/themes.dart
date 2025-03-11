57ac9d4fd86436a2160a7a5d9a41dbd684c957f6
MohammedAli <mohammed.a@devstree.in>
2025-03-11 16:34:15 +0530

    1. Remove dropdown arrows from expandable items

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Themes {
-  // static const kPrimaryColor = Color(0xFFB0A9FB);
-  // static const kAccentColor = Color(0xFFEFE7EE);
-  // static const kLightColor = Color(0xFFF0F0F0);
-
   static const kPrimaryColor = Color(0xFF0E68B1);
   static const kOrangeColor = Color(0xFFFF8D0B);
 
